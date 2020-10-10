5a38051d8b73d03a065839ce34b916b227b8a2b7
gaurav <mr.gaurav245@gmail.com>
2020-10-10 12:38:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splash/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
+
+
+
+    }
+}
